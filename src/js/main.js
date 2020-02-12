d749291b7de5c86767755dc8bcd979ba1ccfd528
Vladislad T <v.trundenko@everad.com>
2020-02-12 17:57:14 +0200

    vlad

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@ import './helpers/postDate';
 import scrollSmooth from './helpers/smooth-scroll.js';
 import example from './modules/example.js';
 import reviews from './modules/reviews.js';
+import clients from './modules/clients.js';
 
 function main () {
   scrollSmooth();
 	example();
 	reviews();
+	clients();
 }
 
 main();
