ce2515849eddb1cdd08f90d043715e088c6ca405
Vladislad T <v.trundenko@everad.com>
2020-02-13 13:24:02 +0200

    vlad

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,14 @@ import scrollSmooth from './helpers/smooth-scroll.js';
 import example from './modules/example.js';
 import reviews from './modules/reviews.js';
 import clients from './modules/clients.js';
+import footer from './modules/footer.js';
 
 function main () {
   scrollSmooth();
 	example();
 	reviews();
 	clients();
+	footer();
 }
 
 main();
