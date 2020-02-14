5abb1696081e7b1df98072babdd2ddc8887be0c5
O.M <a.majstrenko@everad.com>
2020-02-14 13:33:25 +0200

    symptoms desk

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import './helpers/postDate';
 import scrollSmooth from './helpers/smooth-scroll.js';
 // import example from './modules/example.js';
 import menu from './modules/menu.js';
+import symptoms from './modules/symptoms.js';
 import reviews from './modules/reviews.js';
 import clients from './modules/clients.js';
 import footer from './modules/footer.js';
@@ -10,6 +11,7 @@ function main () {
   scrollSmooth();
 	// example();
 	menu();
+	symptoms();
 	reviews();
 	clients();
 	footer();
