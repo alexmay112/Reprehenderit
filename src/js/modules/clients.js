d36bcfa495be5cc0349700d7804d9db34820f7f2
Vladislad T <v.trundenko@everad.com>
2020-02-14 11:26:49 +0200

    vlad

--- src/js/modules/clients.js
+++ src/js/modules/clients.js
@@ -4,6 +4,8 @@ export default () => {
 		slidesToShow: 3,
 		slidesToScroll: 3,
 		arrows: true,
+		autoplay: true,
+		autoplaySpeed: 6000,
 		dots: true,
 		prevArrow: '<svg class="arrow-left"  xmlns="http://www.w3.org/2000/svg" width="40" height="24" viewBox="0 0 40 24"><g><g><path d="M0 11H.51L9.435.151l1.414 1.413L3.086 11H40v2H3.087l7.762 9.435-1.414 1.414L.51 13H0z"/></g></g></svg>',
 		nextArrow: '<svg class="arrow-right" xmlns="http://www.w3.org/2000/svg" width="40" height="24" viewBox="0 0 40 24"><g><g><path d="M0 11h36.914l-7.763-9.436L30.565.151l8.926 10.85H40v2h-.51l-8.925 10.848-1.414-1.414L36.913 13H0z"/></g></g></svg>',
