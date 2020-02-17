84f28d8bd02e7685233326a0c4ae871a7018cc09
O.M <a.majstrenko@everad.com>
2020-02-17 17:06:57 +0200

    teamlead fix

--- src/js/modules/wPopup.js
+++ src/js/modules/wPopup.js
@@ -1,21 +1,24 @@
-
-
-
-
 export default () => {
-	$(document).ready(function () {
+  $(document).ready(function () {
+
+    document.querySelector('.promo__pay').style.bottom = '0' + 'px';
 
     $(window).on("scroll", function () {
-       
-        if (($(window).scrollTop() > $('.symptoms').offset().top) && ($(window).scrollTop() < $('.fakes').offset().top)) {
-            $(".wPopup").fadeIn();
-            
-        } else {
-            $(".wPopup").fadeOut();
-            
-        }
+      if (($(window).scrollTop() > $('.symptoms').offset().top) && ($(window).scrollTop() < $('.fakes').offset().top)) {
+        $(".wPopup").fadeIn();
 
+        // window.addEventListener(`resize`, event => {
+        if (document.documentElement.clientWidth < 768) {
+          document.querySelector('.promo__pay').style.bottom = '55' + 'px';
+        } else {
+          document.querySelector('.promo__pay').style.bottom = '91' + 'px';
+        }
+        // }, false);
+
+      } else {
+        $(".wPopup").fadeOut();
+        document.querySelector('.promo__pay').style.bottom = '0' + 'px';
+      }
     });
-   
-})
+  })
 };
