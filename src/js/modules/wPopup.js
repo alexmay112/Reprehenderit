725003a731b466095043f36983b70876d75a6c3f
Vladislad T <v.trundenko@everad.com>
2020-02-20 13:11:58 +0200

    fix menu

--- src/js/modules/wPopup.js
+++ src/js/modules/wPopup.js
@@ -1,24 +1,11 @@
 export default () => {
-  $(document).ready(function () {
-
-    document.querySelector('.promo__pay').style.bottom = '0' + 'px';
-
-    $(window).on("scroll load", function () {
+    $(window).on("scroll load", function () {	
       if (($(window).scrollTop() > $('.symptoms').offset().top) && ($(window).scrollTop() < $('.fakes').offset().top)) {
-        $(".wPopup").fadeIn();
-
-        // window.addEventListener(`resize`, event => {
-        if (document.documentElement.clientWidth < 768) {
-          document.querySelector('.promo__pay').style.bottom = '56' + 'px';
-        } else {
-          document.querySelector('.promo__pay').style.bottom = '88' + 'px';
-        }
-        // }, false);
-
+				$(".wPopup").fadeIn();
+				$('.promo__pay').css({"bottom":  $(".wPopup").outerHeight()});
       } else {
-        $(".wPopup").fadeOut();
-        document.querySelector('.promo__pay').style.bottom = '0' + 'px';
+				$(".wPopup").fadeOut();
+				$('.promo__pay').css({"bottom":  0});
       }
     });
-  })
 };
